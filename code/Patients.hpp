ba76e69e465e4b37ba98181261b6444bf90d66bb
phatnguyen02 <ngphat1211@gmail.com>
2021-09-27 21:30:55 +0700

    sửa lại các hàm cho tree

--- code/Patients.hpp
+++ code/Patients.hpp
@@ -22,9 +22,9 @@ class BST {
         bool existPatient(); //kiểm tra trùng id
         bool validInfection(); //kiểm tra trùng Id F0 là “NO”, các id của F khác k đc trùng nhau
         Node* add(Node* root, Patient val);
-        void erase(); //xóa bệnh nhân(dùng hàm find và erase có sẵn)
+        Node* search(Node* root, Patient val);//tìm kiếm
+        Node* erase(Node* root, Patient val);//xóa 
         void edit(); //sửa bệnh nhân
-        void findPatient(); //dùng hàm find có sẵn
         void sort(); //dùng hàm sort có sẵn kết hợp thêm 1 class Compare để so sánh và sắp xếp
         void statistics(); //thống kê theo nơi điều trị
         void F(); //thống kê các F (nếu có map thì dùng, k thì thoi)
